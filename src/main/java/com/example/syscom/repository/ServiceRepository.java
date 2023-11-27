c267a2e501d2a5780ad981d2eb37ddd713d54669
RDnante <119560912+RDnante@users.noreply.github.com>
2023-11-27 09:16:39 +0600

    farany 2

--- src/main/java/com/example/syscom/repository/ServiceRepository.java
+++ src/main/java/com/example/syscom/repository/ServiceRepository.java
@@ -2,6 +2,8 @@ package com.example.syscom.repository;
 
 import com.example.syscom.model.ServiceC;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ServiceRepository extends JpaRepository<ServiceC,Integer> {
+//    @Query("from ")
 }
